1d719fde8a81eb9a6bf8ce5929fda0c9ee331f17
athav2823 <atharvaneware@gmail.com>
2024-05-15 21:17:06 +0530

    todo list is added

---
+++ javascript/todo.js
@@ -0,0 +1,39 @@
+let todoList = [];
+
+const listFromLocalStorage = localStorage.getItem("todoList");
+if (listFromLocalStorage) {
+    todoList = JSON.parse(listFromLocalStorage);
+    renderToDoList();
+}
+
+
+function add() {
+    const inputElement = document.getElementById('todo-element');
+    todoList.push(inputElement.value);
+
+    localStorage.setItem("todoList", JSON.stringify(todoList));
+
+    inputElement.value = "";
+
+    renderToDoList();
+}
+
+function renderToDoList() {
+    const listContainer = document.getElementById('list-container');
+    listContainer.innerHTML = "";
+
+    for (const todoItem of todoList) {
+        listContainer.innerHTML += `<div class="todoItem"> <input type="checkbox">${todoItem}</div>`;
+    }
+
+}
+function clear() {
+    const inputElement = document.getElementById('todo-element');
+    todoList.push(inputElement.value);
+
+    // localStorage.setItem("todoList", JSON.stringify(todoList));
+
+    inputElement.value = "";
+
+
+}
